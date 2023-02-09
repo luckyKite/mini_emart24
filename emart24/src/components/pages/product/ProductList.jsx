44cd1b791622f9b1637925d9652656a28032c3bc
YJ Ahn <beat1103@gmail.com>
2023-02-09 15:33:34 +0900

    event

--- emart24/src/components/pages/product/ProductList.jsx
+++ emart24/src/components/pages/product/ProductList.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import style from "./ProductList.module.css";
 
 function ProductList({product}) {
 
   const userId = 1;
+  console.log(product)
+
+  const [productData, setProductData] = useState();
 
   const handleAddCart = () => {
     fetch('http://localhost:3001/carts', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        productId: product.id,
+        productId: product,
         userId: userId,
         qty: 1
       })
@@ -25,26 +28,40 @@ function ProductList({product}) {
     .catch(err => console.error(err));
   }
 
+  useEffect(()=> {
+    fetch(`http://localhost:3001/products/${product}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setProductData(data);
+    }).catch(err => console.error(err));
+  },[product])
+
 
   return (
     <div className={style.productListWrap}>
-      <div className={style.rating}>
-        <p>👍{product.rating}</p>
-      </div>
-      <div className={style.image}>
-        <Link to= {`/productDetail/${product.id}`}>
-          <img src={product.thumbnail} alt={product.description} />
-        </Link>   
-      </div>
-      <div className={style.productInfo}>
-        <p className={style.name}>{product.name}</p>
-        <p className={style.price}>{product.price}원</p>
-      </div>
-      <div className={style.cartBtnWrap}>
-        <button onClick={handleAddCart} className={style.cartBtn}>
-          담기
-        </button>
-      </div>
+      { productData && (
+        <>
+        <div className={style.rating}>
+          <p>👍{productData.rating}</p>
+        </div>
+        <div className={style.image}>
+          <Link to= {`/productDetail/${productData.id}`}>
+            <img src={productData.thumbnail} alt={productData.description} />
+          </Link>   
+        </div>
+        <div className={style.productInfo}>
+          <p className={style.name}>{productData.name}</p>
+          <p className={style.price}>{productData.price}원</p>
+        </div>
+        <div className={style.cartBtnWrap}>
+          <p onClick={handleAddCart} className={style.cartBtn}>
+            담기
+          </p>
+        </div>
+        </>
+      )} 
+      
     </div>
     );
 }
