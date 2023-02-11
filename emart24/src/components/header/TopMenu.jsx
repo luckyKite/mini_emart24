b7417553dcc1a371ec1436ec9ab766846261fa10
채란 <nemo20435@gmail.com>
2023-02-12 02:58:01 +0900

    장바구니 상품종류 숫자표시 배너 생성

--- emart24/src/components/header/TopMenu.jsx
+++ emart24/src/components/header/TopMenu.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import style from "./TopMenu.module.css";
 import Lottie from "lottie-react";
 import cart from "../../image/cart.json";
+import { CartCountState } from '../state/CartCountState';
+import { useRecoilState } from 'recoil';
 
 function TopMenu() {
+
+  const userId = 1;
+  const [cartQty, setCartQty] = useRecoilState(CartCountState); 
+
+  useEffect( () => {
+    fetch(`http://localhost:3001/carts?userId=${userId}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setCartQty(data.length)
+    });
+  }, [userId]);
+
+
   return (
     <ul className={style.topMenu}>
       <li><Link to={'/product'}>상품</Link></li>
       <li><Link to={'/event'}>이벤트</Link></li>
       <li>
-      <Link to={'/cart'}>
-        <Lottie animationData={cart} style={{width: "100px", display: "inline-block", }}></Lottie>
-      </Link>
+        <div className='cartIcon'>
+          <Link to={'/cart'}>
+            <Lottie animationData={cart} style={{width: "100px", display: "inline-block", }}></Lottie>
+          </Link>
+          <p className={style.qtyBadge}>담긴 상품종류: {cartQty}</p>
+          </div>
       </li>
     </ul>
    );
