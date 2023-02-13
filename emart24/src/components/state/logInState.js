06c79c79e9cddf1ff54af8e1552abcb78022e89b
채란 <nemo20435@gmail.com>
2023-02-13 18:10:48 +0900

    logInState 수정

--- emart24/src/components/state/logInState.js
+++ emart24/src/components/state/logInState.js
@@ -1,7 +1,9 @@
 import { atom } from 'recoil';
+import { recoilPersist} from 'recoil-persist';
 
+const { persistAtom } = recoilPersist();
 export const logInState = atom({
   key: 'logInState',
   default: false,
+  effects_UNSTABLE: [persistAtom]
 });
-
