9ad61bd3e80752e49316f43602767a7603962909
beat1103 <beat1103@gmail.com>
2023-02-14 00:56:56 +0900

    login

--- emart24/src/components/pages/join/Join.jsx
+++ emart24/src/components/pages/join/Join.jsx
@@ -7,24 +7,15 @@ import Swal from "sweetalert2";
 function Join() {
 
   const navigate = useNavigate();
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [passwordChk, setPasswordChk] = useState();
-  const [isPasswordSame, setIsPasswordSame] = useState(false);
-  const [isEmailchk, setIsEmailchk] = useState(false);
 
-  useEffect(() => {
-    if (password && passwordChk) {
-      setIsPasswordSame(false);
-      Swal.fire('', '비밀번호가 일치하지 않습니다. 다시 확인해주세요!');
-      if (password === passwordChk) {
-        setIsPasswordSame(true);
-        Swal.fire('', '비밀번호가 확인되었습니다. 회원가입을 한번 더 눌러주세요^^');
-      }
-    }
-  }, [password, passwordChk]);
-
+  const [inputData, setInputData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    isDuplicateEmail: false,
+  });
+  
   // 이메일 정규식 테스트
   const emailCheck = (email) => {
     const emailForm = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
@@ -33,101 +24,81 @@ function Join() {
 
   // 비밀번호 정규식 테스트
   const passwordCheck = (password) => {
-    let result = false;
     const passwordForm = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
-    if (password.match(passwordForm) === null) {
-      Swal.fire('', '비밀번호는 영문, 숫자, 특수문자 포함 8~20자 입니다.')
-      //console.log('비번양식 틀림')
-    } else {
-      //console.log('비번 양식 ok');
-      // Swal.fire('','비밀번호를 아래에 한번 더 입력해주세요^^'); //성공하고 한번 더 나옴
-      result = true;
-    }
-    return result;
+    return password.match(passwordForm)
   }
 
-  const handlerName = (e) => {
-    if (e.target.value) {
-      setName(e.target.value);
-    }
-  }
+  const handleInputData = (e) => {
+    const { name, value } = e.target;
+    setInputData({
+      ...inputData,
+      [name]: value,
+    });
+  };
 
-  const handlerEmail = (e) => {
-    if (emailCheck(e.target.value)) {
-      setEmail(e.target.value);
-      setIsEmailchk(false);
+  const hanlderJoinBtn = (e) => {
+    e.preventDefault();
+    if( inputData.name === '' || inputData.email === '' || inputData.password === '' || inputData.confirmPassword === ''){
+      return alert('빈칸을 모두 입력해주세요!');
     }
-  }
-
-  const handlerPassword = (e) => {
-    if (passwordCheck(e.target.value)) {
-      setPassword(e.target.value);
+    if(!emailCheck(inputData.email)){
+      return alert('이메일 형식이 올바르지 않습니다.');
     }
-  }
-
-  const handlerPasswordChk = (e) => {
-    if (passwordCheck(e.target.value)) {
-      setPasswordChk(e.target.value);
+    if(!passwordCheck(inputData.password)){
+      return alert('비밀번호 형식이 올바르지 않습니다.');
     }
-  }
-
-  const hanlderJoinBtn = () => {
-    console.log(name, email, password, passwordChk, isEmailchk);
-    // 모든 값이 있고
-    if(name && email && isPasswordSame && isEmailchk) {
-        fetch('http://localhost:3001/users', {
-          method: 'POST',
-          headers: {"Content-type" : "application/json"},
-          body: JSON.stringify({
-            name: name,
-            email: email,
-            password: password
-          })
-        })
-        .then(res => {
-          res.json();
-          //setIsPasswordSame()
-        })
-        .then(data =>
-          console.log("입력된데이터", data))
-        .catch(err => console.error(err));
+    if(inputData.password !== inputData.confirmPassword){
+      return alert('비밀번호가 서로 일치하지 않습니다.');
+    }
+    if(!inputData.isDuplicateEmail){
+      return alert('이메일 중복확인을 해주세요.');
+    }
+    fetch('http://localhost:3001/users', {
+      method: 'POST',
+      headers: {"Content-type" : "application/json"},
+      body: JSON.stringify({
+        name: inputData.name,
+        email: inputData.email,
+        password: inputData.password,
+      })
+    })
+    .then(res => {
+      if(res.ok){
+        alert('회원가입이 완료되었습니다.');
+        navigate('/');
       }
-      alert('회원가입을 성공했습니다!');
-      navigate('/login');
-      //console.log(passwordMatch(password,passwordChk))
+    })
+    .catch(err => console.error(err));
   }
 
   /** 이메일 중복확인  -> 중복확인은 되는데 가입이 되네요...*/
   const emailChk = () => {
-    fetch(`http://localhost:3001/users?email=${email}`)
+    fetch(`http://localhost:3001/users?email=${inputData.email}`)
       .then((res) => res.json())
       .then((data) => {
         if (data.length === 0) {
           alert("사용가능 이메일입니다");
-          setIsEmailchk(true);
+          setInputData({
+            ...inputData,
+            isDuplicateEmail: true
+          });
         } else {
           alert("중복된 이메일입니다");
-          setIsEmailchk(false);
         }
       });
   };
   
-  
-
-
-
-
   return (
     <div className='container'>
-      <div className={style.joinFrom}>
+      <form className={style.joinFrom} onSubmit={hanlderJoinBtn}>
         <p className={style.comment}>emart24에 가입하시고 혜택을 누리세요! </p>
-        <p><input type="text" placeholder='이름' onBlur={handlerName} /></p>
-        <p><input type="text" placeholder='이메일' onBlur={handlerEmail} /></p>
-        <button onClick={emailChk}>이메일 확인</button>
-        <p><input type="password" placeholder='비밀번호는 영문, 숫자, 특수문자 포함 8~20자 입니다.' onBlur={handlerPassword} /></p>
-        <p><input type="password" placeholder='비밀번호를 한번 더 입력 해 주세요.' onBlur={handlerPasswordChk} /></p>
-        <button onClick={hanlderJoinBtn} className={style.joinBtn}>회원가입</button>
-      </div>
+        <input type="text" placeholder='이름' name='name' onChange={handleInputData}/>
+        <input type="text" placeholder='이메일' name='email' onChange={handleInputData}/>
+        <p onClick={emailChk}>이메일 확인</p>
+        <input type="password" placeholder='비밀번호는 영문, 숫자, 특수문자 포함 8~20자 입니다.' name='password' onChange={handleInputData}/>
+        <input type="password" placeholder='비밀번호를 한번 더 입력 해 주세요.' name='confirmPassword' onChange={handleInputData}/>
+        <button type="submit" className={style.joinBtn}>회원가입</button>
+      </form>
     </div>
   );
 }
