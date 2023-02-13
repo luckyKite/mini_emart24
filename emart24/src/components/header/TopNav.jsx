99c417e3c4bd995acdbf4e22de7aa77abf512f16
채란 <nemo20435@gmail.com>
2023-02-13 09:27:34 +0900

    로그인 되었을 때 상태바 수정

--- emart24/src/components/header/TopNav.jsx
+++ emart24/src/components/header/TopNav.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import style from "./TopNav.module.css";
 
+import { logInState } from '../state/logInState';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+
 function TopNav() {
-  return ( 
-    <ul className={style.topNav}>
-      <li><Link to={'/join'}>회원가입</Link></li>
-      <li><Link to={'/login'}>로그인</Link></li>
+  const [logInData, setLogInData] = useRecoilState(logInState);
+
+  return (
+    <ul className={style.topNav}> {
+      logInData ?
+        <>
+          <li><Link to={'/member'}>회원정보</Link></li>
+          <li onClick={()=> setLogInData(false)}>로그아웃</li>
+        </> 
+        : 
+        <>
+          <li><Link to={'/join'}>회원가입</Link></li>
+          <li><Link to={'/login'}>로그인</Link></li>
+        </>
+    }
     </ul>
-   );
+  );
 }
 
 export default TopNav;
